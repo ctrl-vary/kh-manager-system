7ae364d99ed6eb077c94a405eb4745839a40a9cc
Sherlockmmc <731853112@qq.com>
2021-03-27 16:14:11 +0800

    联系人编辑

--- src/main/java/com/hqyj/controller/KhController.java
+++ src/main/java/com/hqyj/controller/KhController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -114,5 +115,16 @@ public class KhController {
         String info=khService.del(kh);
         map.put("info",info);
         return map;
+    }
+
+    /**
+     *
+     * @param kh
+     * @return
+     */
+    @PostMapping("/linkMan/update")
+    public String update(kh kh){
+        khService.update(kh);
+        return khService.update(kh);
     }
 }
